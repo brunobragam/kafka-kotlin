15fe4f7af46c2e1df947db88c521896e3faec944
bruno.braga <bruno.braga@global.komatsu>
2023-05-17 15:44:37 -0300

    First Commit

---
+++ src/test/kotlin/com/example/kafkakotlin/KafkaKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kafkakotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
